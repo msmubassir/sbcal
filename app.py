9dd84550a8f6d93f139b9f31baf13b7937b71cd3
msmubassir <125449220+msmubassir@users.noreply.github.com>
2024-09-03 09:37:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,5 @@ def day(day):
     return render_template(f'{day}day.html', day=day)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
